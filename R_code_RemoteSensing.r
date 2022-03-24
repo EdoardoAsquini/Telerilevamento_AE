d1eec6b9a28ea4bba4cd0874d49c383505e975c8
EdoardoAsquini <100956837+EdoardoAsquini@users.noreply.github.com>
2022-03-24 16:21:26 +0100

    Update R_code_RemoteSensing.r

--- R_code_RemoteSensing.r
+++ R_code_RemoteSensing.r
@@ -1,18 +1,26 @@
 #Questo è il primo script che useremo a lezione
 #install.packages("raster")
+#install.packages("gglpot2") #virgolette per "uscire" da R (ovvero legge file esterni al programma R); quello dentro le parentesi si chiama ARGOMENTO
+#install.packages("rgdal")
+#install.packages("RStoolbox")
 library(raster)
+library(Rstoolbox)
+
 setwd("C:/lab/") #setta la cartella di lavoro, pesca sempre da qui
 Landsat_2011 <- brick("C:/lab/Landsat_p224r63/p224r63_2011.grd")
 Landsat_2011
 plot(Landsat_2011) #posso usare la f.ne plot perchè i dati sono dei valori di punti con delle coordinate x e y; R mette legenda standard 
 grigi <- colorRampPalette(c("black", "grey", "light grey")) (100) #crea una legenda con i colori selezionati; fa passaggio graduale tra i colori pari a (100), in questo caso
 plot(Landsat_2011, col=grigi)
+
 # b1= blue
 # b2= green
 #b3= red
 #b4= NIR
+
 blue <- colorRampPalette(c("blue4", "blue", "cornflowerblue")) (100) #palette dei blu
 plot(Landsat_2011$B1_sre, col=blue)
+
 green <- colorRampPalette(c("dark green", "green", "light green")) (100)
 plot(Landsat_2011$B2_sre, col=green)
 
