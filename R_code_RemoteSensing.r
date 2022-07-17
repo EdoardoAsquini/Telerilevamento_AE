0e2ba12911c1b1b92ccd9e38960312a0a5d5d1e8
EdoardoAsquini <100956837+EdoardoAsquini@users.noreply.github.com>
2022-07-17 16:25:11 +0200

    Update R_code_RemoteSensing.r

--- R_code_RemoteSensing.r
+++ R_code_RemoteSensing.r
@@ -47,11 +47,54 @@ plotRGB(Landsat_2011, r=4, g=3, b=2, stretch="lin") #NIR in the Red band
 plotRGB(Landsat_2011, r=3, g=4, b=2, stretch="lin") #NIR in the Green band
 plotRGB(Landsat_2011, r=3, g=2, b=4, stretch="lin") #NIR in the Blue band, soil becomes yellow
 
-Landsat_1988 <- brick("C:/lab/Landsat_p224r63/p224r63_1988.grd") #carica l'immagine del 1988
+Landsat_1988 <- brick("C:/lab/Landsat_p224r63/p224r63_1988.grd") #1988 image taken by Landsat
 Landsat_1988
 
 par(mfrow=c(2,1)) #compares the two images, NIR in the red band
 plotRGB(Landsat_1988, r=4, g=3, b=1, stretch="lin")
 plotRGB(Landsat_2011, r=4, g=3, b=1, stretch="lin")
 
+l1992 <- brick("C:/lab/defor1.jpg")
+l1998 <- brick("C:/lab/defor2.jpg")
 
+plotRGB(l1992, r=1, g=3, b=2, stretch="lin")
+plotRGB(l1998, r=1, g=3, b=2, stretch="lin")
+
+par(mfrow=c(2,1))
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plotRGB(l1998, r=1, g=2, b=3, stretch="lin")
+
+DVI1992 <- l1992[[1]] - l1992[[2]] #Difference Vegetation Inedex=Nir-Red; used as a proxy for vegetation health status
+DVI1998 <- l1998[[1]] - l1998[[2]]
+
+NDVI1992 <- DVI1992/(l1992[[1]] + l1992[[2]])
+NDVI1998 <- DVI1998/(l1998[[1]] + l1998[[2]])
+
+ndvi <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+
+plot(DVI1992, col=ndvi)
+plot(DVI1998, col=ndvi)
+plot(NDVI1992, col=ndvi)
+plot(NDVI1998, col=ndvi)
+
+DVIdiff <- colorRampPalette(c("blue", "white", "red")) (100)
+
+DVI_diff <- DVI1992 - DVI1998
+plot(DVI_diff, col=DVIdiff)
+
+par(mfrow=c(2,1))
+plotRGB(Landsat_1988, r=1, g=2, b=3, stretch="lin")
+plot(NDVI1992, col=ndvi)
+
+par(mfrow=c(2,1))
+plot(NDVI1992, col=ndvi)
+plot(NDVI1998, col=ndvi)
+
+library(RStoolbox)
+
+SI1992 <- spectralIndices(l1992, green = 3, red = 2, nir = 1)
+plot(SI1992, col=ndvi)
+
+library(rasterdiv)
+
+plot(copNDVI) #mean NDVI from Copernicus data, function from rasterdiv package
